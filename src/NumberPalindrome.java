72789477260121e95f377af9ee23a555599c6050
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-09-29 16:19:09 -0400

    First Commit

---
+++ src/NumberPalindrome.java
@@ -0,0 +1,38 @@
+public class NumberPalindrome {
+
+    public static void main(String[] args) {
+
+        isPalindrome(456521);
+        isPalindrome(11);
+        isPalindrome(-252);
+        isPalindrome(98589);
+        isPalindrome(-1265789524);
+        isPalindrome(-13231);
+
+
+    }
+    public static boolean isPalindrome(int number){
+
+        int reverse = 0;
+        int original = number;
+        while (original != 0) {
+
+            int lastDigit = original % 10;
+            reverse = (reverse * 10) + lastDigit;
+
+
+            original /= 10;
+        }
+
+        if (number == reverse) {
+            System.out.println(number + " is a palindrome");
+            return true;
+        }
+        else {
+            System.out.println(number + " is not a palindrome");
+            return false;
+        }
+
+    }
+
+}
